2430e1577291c9cb40c5b5f1c1fb4857b052d359
plungerman <plungerman@gmail.com>
2020-04-06 08:53:28 -0500

    removed .gitignore from css/ and js/ and added the .css and .js files

---
+++ djthia/static/js/main.js
@@ -0,0 +1,121 @@
+// Opera 8.0+
+var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+// Firefox 1.0+
+var isFirefox = typeof InstallTrigger !== 'undefined';
+// Safari 3.0+ "[object HTMLElementConstructor]"
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+// Edge 20+
+var isEdge = !isIE && !!window.StyleMedia;
+// Chrome 1 - 71
+var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+// Blink engine detection
+var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+if (isFirefox==false && isChrome==false && isOpera==false && isSafari==false) {
+    alert('Please use the FireFox, Chrome, Safari 12+, or Opera Browser');
+}
+
+/* spinner: instantiate */
+var opts = {
+  lines: 13, // The number of lines to draw
+  length: 20, // The length of each line
+  width: 10, // The line thickness
+  radius: 30, // The radius of the inner circle
+  corners: 1, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#000', // #rgb or #rrggbb or array of colors
+  speed: 1, // Rounds per second
+  trail: 60, // Afterglow percentage
+  shadow: false, // Whether to render a shadow
+  hwaccel: false, // Whether to use hardware acceleration
+  className: 'search-results', // The CSS class to assign to spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: '50px', // Top position relative to parent in px
+  left: 'auto' // Left position relative to parent in px
+};
+var target = document.getElementById('alert-container');
+var spinner = new Spinner(opts).spin(target);
+spinner.stop(target);
+
+$(function() {
+  $(".required > input").addClass("required");
+  $(".required > select").addClass("required");
+  $(".required > textarea").addClass("required");
+  $(".warning > input").addClass("error");
+  $(".warning > select").addClass("error");
+  $(".warning > textarea").addClass("error");
+  $(".required > ul").parent().parent().find('h3').addClass("required");
+  $('#copyright').after("&nbsp;" + (new Date).getFullYear());
+  /* bootstrap tool tip */
+  $('[data-toggle="tooltip"]').tooltip();
+  /* print page */
+  $('#print').click(function() {
+    window.print();
+    return false;
+  });
+  /* wysiwyg for textarea fields */
+  var $trumBowygDict = {
+    btns: [
+      ['formatting'], ['strong', 'em', 'del'], ['link'],
+      ['unorderedList', 'orderedList'], ['horizontalRule'], ['viewHTML'],
+    ],
+    tagsToRemove: ['script', 'link'], urlProtocol: true,
+    removeformatPasted: true, semantic: true, autogrow: true, resetCss: true
+  };
+  $('#id_body').trumbowyg($trumBowygDict);
+  /* clear django cache object by cache key and refresh content */
+  $('.clear-cache').on('click', function(e){
+    e.preventDefault();
+    var $dis = $(this);
+    var $cid = $dis.attr('data-cid');
+    var $target = '#' + $dis.attr('data-target');
+    var $html = $dis.html();
+    $dis.html('<i class="fa fa-refresh fa-spin"></i>');
+    $.ajax({
+      type: 'POST',
+      url: $clearCacheUrl,
+      data: {'cid':$cid},
+      success: function(data) {
+        $.growlUI("Cache", "Clear");
+        $($target).html(data);
+        $dis.html('<i class="fa fa-refresh"></i>');
+      },
+      error: function(data) {
+        $.growlUI("Error", data);
+      }
+    });
+    return false;
+  });
+  /* datatables initialization */
+  var gearupTable = $('#data-table').DataTable({
+    'lengthMenu': [
+      [25, 50, 100, 250, 500, 1000, 2000, -1],
+      [25, 50, 100, 250, 500, 1000, 2000, 'All']
+    ],
+    dom: 'lfrBtip',
+    buttons: [
+      'csv', 'excel'
+    ]
+  });
+  /* modal for donations */
+  $('#id_donation').change(function() {
+    var opval = $(this).val();
+    if(opval=='Yes'){
+      $('#donation-modal').modal('show');
+    }
+  });
+  /* override the submit event for the alert form to handle some things */
+  $('form#profile').submit(function(){
+    /* check textarea for just br tag */
+    $('textarea').each(function(){
+      if (this.value == '<br>') {
+          this.value = '';
+      }
+    });
+    // disable submit button after users clicks it
+    $(this).children('input[type=submit]').attr('disabled', 'disabled');
+  });
+});
