20fd0f412de2fc38d3b29de0eda06737464da71d
plungerman <plungerman@gmail.com>
2020-04-15 12:28:23 -0500

    removed get_status() and merged it with get_student

--- djthia/core/utils.py
+++ djthia/core/utils.py
@@ -55,11 +55,11 @@ def get_orgs(cid):
     return orgs
 
 
-def get_status(cid):
+def get_student(cid):
     """Determine if the student is eligible to participate."""
-    key = 'gearup_status_{0}'.format(cid)
-    status = cache.get(key)
-    if not status:
+    key = 'gearup_student_{0}'.format(cid)
+    student = cache.get(key)
+    if not student:
         phile = os.path.join(settings.BASE_DIR, 'sql/gearup.sql')
         with open(phile) as incantation:
             sql = '{0} AND Program_Enrollment_Record.id={1}'.format(
@@ -68,25 +68,8 @@ def get_status(cid):
         connection = get_connection()
         with connection:
             row = xsql(sql, connection, key=settings.INFORMIX_DEBUG).fetchone()
-            status = False
             if row:
-                status = True
-            cache.set(key, status)
-
-    return status
-
-
-def get_student(cid):
-    """Fetch student data."""
-    key = 'student_vitals_{0}'.format(cid)
-    student = cache.get(key)
-    if not student:
-        with get_connection() as connection:
-            cursor = connection.cursor().execute(VITALS(cid=cid))
-            # obtain the column names
-            columns = [column[0] for column in cursor.description]
-            row = cursor.fetchone()
-            if row:
-                student = dict(zip(columns, row))
+                student = row
                 cache.set(key, student)
+
     return student
