954f0ea496660a95251ab082d3f0c861bfd0723d
plungerman <plungerman@gmail.com>
2020-05-21 10:05:45 -0500

    new feature that will send all thank you notes via email to their intended recipients

--- djthia/bin/thank_you.py
+++ djthia/bin/thank_you.py
@@ -13,7 +13,7 @@ from djthia.gearup.models import Annotation
 from djtools.utils.mail import send_mail
 
 # env
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djthia.settings.shell")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djthia.settings.shell')
 
 DEBUG = settings.DEBUG
 
@@ -22,18 +22,18 @@ def main():
     """Send emails to recipients of thank you notes."""
     notes = Annotation.objects.all()[:3]
     for note in notes:
-        print(note.created_by, note.status)
         if note.status:
             to = [note.recipients.all()[0].email]
+            frum = note.created_by.email
             if DEBUG:
-                #to = [settings.MANAGERS[0][1]]
-                to = ['akoenig1@carthage.edu', 'eyoung@carthage.edu']
+                note.to = to
+                note.frum = frum
+                to = [settings.MANAGERS[0][1]]
+                print(to)
+                print(note.created_by, note.status)
             subject = "A Thank You Note from {0} {1}".format(
                 note.created_by.first_name, note.created_by.last_name,
             )
-            frum = note.created_by.email
-            note.to = to
-            note.frum = frum
             send_mail(None, to, subject, frum, 'gearup/notes_email.html', note)
             note.status = False
             note.save()
