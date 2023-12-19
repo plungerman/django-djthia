50d30a03c2a3e07a7b5f664d94765db2e8a1e567
plungerman <plungerman@gmail.com>
2023-12-19 12:41:40 -0600

    using 'shell' instead of 'production' in the wsgi file since shell.py points to the environment e.g. staging, production, etc

--- djthia/wsgi_default.py
+++ djthia/wsgi_default.py
@@ -12,7 +12,7 @@ from django.core.wsgi import get_wsgi_application
 sys.path.append('/d2/python_venv/3.8/djthia/lib/python3.8/')
 sys.path.append('/d2/python_venv/3.8/djthia/lib/python3.8/site-packages/')
 # django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djthia.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djthia.settings.shell')
 os.environ.setdefault('PYTHON_EGG_CACHE', '/var/cache/python/.python-eggs')
 os.environ.setdefault('TZ', 'America/Chicago')
 # informix
